c9b26793f455c8eb861d27d8ca86ac1abb188e55
Daniel Cornelio <jdcornelio89@gmail.com>
2020-07-26 21:50:01 -0500

    propiedades computadas

---
+++ 01.vueEsencial/05.propiedadesComputadas/js/main.js
@@ -0,0 +1,39 @@
+const vm = new Vue({
+    el: 'main',
+    
+    data: {
+        mensaje: 'Hola Mundo :V',
+        nuevaTarea: 'null',
+        tareas: [
+            {titulo: 'Aprender Vue', prioridad: true, antiguedad:23},
+            {titulo: 'Aprender ES6', prioridad: false, antiguedad:135},
+            {titulo: 'Publicar algo todos los dias', prioridad: true, antiguedad:378},
+        ],
+    },
+    methods:{
+        agregarTarea(){
+            //this hace referencia a la instancia Vue
+            this.tareas.unshift({
+                titulo: this.nuevaTarea,
+                prioridad: false,
+                antiguedad: 0,
+            });
+            this.nuevaTarea=null;
+        }
+    },
+    computed:{
+        mensajeAlReves(){
+            return this.mensaje.split('').reverse().join('');
+        },
+        tareasConPrioridad(){
+            
+        }
+    }
+});
+
+//vanilla js
+// function agregarTarea(){
+//     const input = document.querySelector('input[type=text]');
+//     vm.tareas.unshift(input.value);
+//     input.value='';
+// }
